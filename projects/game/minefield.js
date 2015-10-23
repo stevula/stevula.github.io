733d7359488993c39fd45b57412ffef7e46e9b78
Steven Broderick <brodericksteven@gmail.com>
2015-10-23 13:34:12 -0700

    Refactor minefield

--- projects/game/minefield.js
+++ projects/game/minefield.js
@@ -1,52 +1,17 @@
- // Design Basic Game Solo Challenge
+ // MINEFIELD
 
-// This is a solo challenge
-
-// Your mission description:
-// Overall mission: uncover all the tiles without hitting a mine.
-// Goals: identify safe tiles to click.
-// Characters: the player.
-// Objects: tiles (mine tiles and safe tiles). Possibly flags after MVP.
-// Functions:
-//    newBoard => randomize tiles
-//    clickCell => uncovers the tile
-//    mine => game over
-//    safe => display number of mines touching
-//    solved => check if board solved (victory)
-//    settings (optional function) => configure size of board and # of mines
-//    flag (optional function) => mark tile with flag (suspected mine)
-
-// Pseudocode
-//  CREATE HTML/CSS table with 5 rows and 5 columns, invisible text.
-
-//  DECLARE function newBoard
-//    FOR EACH cell in table
-//      ASSIGN variable isMine to undefined
-//      GENERATE random number between 0 - 4.
-//      IF 0
-//        SET isMine to true
-//      ELSE
-//        SET isMine to false
-//        ASSIGN variable minesTouching equal to 0
-//    FOR EACH cell in table
-//      IF isMine is equal to true
-//        FOR EACH cell touching (adjacent or diagonal)
-//          INCREMENT minesTouching by 1
-
-
-//  DECLARE function clickCell
-//    FOR cell
-//      SET text to visible
-//      IF isMine is true
-//        PRINT "You died. Game over."
-
-
-//  DECLARE function solved
-//    SET variable solved equal to true
-//    FOR EACH cell in table
-//      IF text is invisible AND isMine is false
-//         SET solved equal to false
-//    RETURN solved
+ // TODO:
+ // convert to canvas
+ // color digits differently based on number/mine
+ // prevent first click from being a mine
+ // show mines remaining
+ // reset button
+ // allow user to configure board
+ // mark bombs with flags
+ // hide 0's
+ // uncover adjacent 0's
+ // add image instead of "X"
+ // better game over / victory message
 
 // ============================================================================
 // Initial Code
@@ -187,29 +152,3 @@ window.onload = function() {
     return solvedStatus;
   }
 };
-
-// Refactored Code
-
-
-
-
-
-// ============================================================================
-// Reflection
-// ============================================================================
-// What was the most difficult part of this challenge?
-//    There were a lot of difficult problems that I had to tackle, but the most
-//    difficult was figuring out how to connect my JS array to the table in the
-//    DOM. It was very difficult to refer to specific elements in the HTML
-//    without giving them each an ID, which I didn't want to do since there
-//    are 25 cells by default and I wanted to make it easy to resize.
-
-// What did you learn about creating objects and functions that interact with one another?
-//    Everything, including HTML elements and the page and the window, are JS
-//    objects.
-
-// Did you learn about any new built-in methods you could use in your refactored solution? If so, what were they and how do they work?
-//
-
-// How can you access and manipulate properties of objects?
-//    With dot notation or bracket notation.
